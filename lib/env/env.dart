cef5734c033d9b99da0456e3d023b1015aa37d6e
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-07-14 21:23:44 +0900

    追加: 環境変数をenvファイルに定義して使えるよう設定

---
+++ lib/env/env.dart
@@ -0,0 +1,10 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'INITIAL_URL')
+  static const String initialUrl = _Env.initialUrl;
+}
